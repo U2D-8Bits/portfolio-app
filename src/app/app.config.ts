03782014890a4670e273f0d0fc379d195a2996b0
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-02 08:35:04 -0500

    chore: update app configuration to include PrimeNG filter match modes and translations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,32 +2,63 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { FilterMatchMode } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
-
   providers: [
-    provideZoneChangeDetection(
-      {
-        eventCoalescing: true
-      }
-    ),
-    provideRouter(
-      routes
-    ),
-    provideClientHydration(
-      withEventReplay()
-    ),
+    provideZoneChangeDetection({
+      eventCoalescing: true,
+    }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({
+      ripple: true,
+      zIndex: {
+        modal: 1100,
+        overlay: 1000,
+        menu: 1000,
+        tooltip: 1100,
+      },
       theme: {
-        preset: Aura
-      }
-    })
-  ]
-
+        preset: Aura,
+      },
+      filterMatchModeOptions: {
+        text: [
+          FilterMatchMode.STARTS_WITH,
+          FilterMatchMode.CONTAINS,
+          FilterMatchMode.NOT_CONTAINS,
+          FilterMatchMode.ENDS_WITH,
+          FilterMatchMode.EQUALS,
+          FilterMatchMode.NOT_EQUALS,
+        ],
+        numeric: [
+          FilterMatchMode.EQUALS,
+          FilterMatchMode.NOT_EQUALS,
+          FilterMatchMode.LESS_THAN,
+          FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
+          FilterMatchMode.GREATER_THAN,
+          FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
+        ],
+        date: [
+          FilterMatchMode.DATE_IS,
+          FilterMatchMode.DATE_IS_NOT,
+          FilterMatchMode.DATE_BEFORE,
+          FilterMatchMode.DATE_AFTER,
+        ],
+      },
+      translation: {
+        accept: 'Aceptar',
+        reject: 'Rechazar',
+      },
+    }),
+  ],
 };
