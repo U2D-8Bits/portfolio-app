c9207d4a870164422f087ef69a21ab0d7a703fc0
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-02 15:38:18 -0500

    chore: integrate section components into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutSectionComponent, ExperienceSectionComponent, HomeSectionComponent, ProjectsSectionComponent, TechnologiesSectionComponent } from './presentation/components';
+
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
+
+    HomeSectionComponent,
+    ExperienceSectionComponent,
+    ProjectsSectionComponent,
+    AboutSectionComponent,
+    TechnologiesSectionComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
