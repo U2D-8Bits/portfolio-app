27af1c2c323092960bd9b0e94331072db4b2e1a9
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-05 16:24:19 -0500

    chore: add experience box component with HTML and CSS files

---
+++ src/app/presentation/ui/experience-box/experience-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-box',
+  imports: [],
+  templateUrl: './experience-box.component.html',
+  styleUrl: './experience-box.component.css'
+})
+export class ExperienceBoxComponent {
+
+}
