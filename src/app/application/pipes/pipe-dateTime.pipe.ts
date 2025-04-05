480d71b39849b18c127d042df34c63cdcc88c268
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-05 17:50:25 -0500

    Agregar pipe personalizado para formatear fechas en el formato 'Mes - año'

---
+++ src/app/application/pipes/pipe-dateTime.pipe.ts
@@ -0,0 +1,21 @@
+//* Este pipe sirve para convertir una fecha a un formato específico, en este caso, 'Month - year'.
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'customDate' })
+export class CustomDatePipe implements PipeTransform {
+  transform(value: Date): string {
+    //? Almacenamos la fecha en una variable de tipo Date
+    const date: Date = new Date(value);
+
+    //? Extraemos el mes y el año de la fecha
+    const monthName: string = date.toLocaleString('default', { month: 'long' });
+    const year: number = date.getFullYear();
+
+    //? Formateamos la fecha en el formato deseado
+    const formatedDate: string = `${monthName} - ${year}`;
+
+    //? Retornamos la fecha formateada
+    return formatedDate;
+  }
+}
