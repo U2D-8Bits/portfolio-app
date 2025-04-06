944a0a4d70bddbd064b9521318c92915f0672d6c
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-05 22:37:00 -0500

    Agregar interfaz ProjectInterface para definir la estructura de los proyectos

---
+++ src/app/core/domain/interfaces/project.interface.ts
@@ -0,0 +1,14 @@
+//************************************************************/
+//* Interfaz de los Proyectos
+//************************************************************/
+
+export interface ProjectInterface {
+
+  name: string;
+  description: string;
+  technologies: string[];
+  image: string;
+  url: string;
+  repository: string;
+  type: 'FullStack' | 'FrontEnd' | 'BackEnd';
+}
