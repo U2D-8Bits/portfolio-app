b85aa87bed420004c3e6535a49b374f59db8b87b
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-05 23:26:55 -0500

    Actualizar imágenes de proyectos en ProjectBoxComponent con enlaces a imágenes externas

--- src/app/presentation/ui/project-box/project-box.component.ts
+++ src/app/presentation/ui/project-box/project-box.component.ts
@@ -19,7 +19,7 @@ export class ProjectBoxComponent implements OnInit {
       name: 'DiabControl App',
       description: 'Plataforma web asíncrona para el seguimiento de pacientes diabéticos, que implementa sistema de notificaciones y recordatorios con el fin de mejorar la adherencia al tratamiento.',
       technologies: ['Angular', 'NestJS', 'PostgreSQL', 'TypeScript'],
-      image: 'assets/images/diabcontrol.png',
+      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       repository: 'https://github.com/U2D-8Bits/diabControl-client',
       url: 'https://diabcontrol.netlify.app/auth/home',
       type: 'FullStack'
@@ -28,7 +28,7 @@ export class ProjectBoxComponent implements OnInit {
       name: 'Diseño de Portfolio Personal',
       description: 'Diseño de Portfolio personal enfocado en resaltar de manera adecuada mi información personal como profesional.',
       technologies: ['Angular','TypeScript'],
-      image: 'assets/images/diabcontrol.png',
+      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       repository: 'https://github.com/U2D-8Bits/portfolio-app',
       url: '',
       type: 'FrontEnd'
@@ -37,7 +37,7 @@ export class ProjectBoxComponent implements OnInit {
       name: 'ClassBuddy',
       description: 'Aplicación web enfocada en la gestión de clases y tareas, que permite a los usuarios el generar material de estudio usando ChatGPT mejorando la experiencia de aprendizaje.',
       technologies: ['Angular', 'Kotlin', 'PostgreSQL', 'TypeScript'],
-      image: 'assets/images/diabcontrol.png',
+      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       repository: 'https://github.com/Homero22/ClassBuddy',
       url: '',
       type: 'FullStack'
@@ -46,7 +46,7 @@ export class ProjectBoxComponent implements OnInit {
       name: 'App AeroBox',
       description: 'Plataforma web para la gestión de archivos en la nube, que permite a los usuarios almacenar y compartir archivos de manera segura y eficiente.',
       technologies: ['NextJS', 'TypeScript'],
-      image: 'assets/images/diabcontrol.png',
+      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       repository: 'https://github.com/U2D-8Bits/nextjs-dashboard-app',
       url: 'https://app-aerobox.vercel.app/login',
       type: 'FrontEnd'
