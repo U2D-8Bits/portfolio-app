da81ad4d22a7eb3d9a8550cdaecd41a77d2d7cf9
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-06 14:48:24 -0500

    Actualizar imágenes de proyectos en ProjectBoxComponent y agregar propiedad bgColor

--- src/app/presentation/ui/project-box/project-box.component.ts
+++ src/app/presentation/ui/project-box/project-box.component.ts
@@ -19,37 +19,41 @@ export class ProjectBoxComponent implements OnInit {
       name: 'DiabControl App',
       description: 'Plataforma web asíncrona para el seguimiento de pacientes diabéticos, que implementa sistema de notificaciones y recordatorios con el fin de mejorar la adherencia al tratamiento.',
       technologies: ['Angular', 'NestJS', 'PostgreSQL', 'TypeScript'],
-      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'assets/images/projects/diabcontrol-app.png',
       repository: 'https://github.com/U2D-8Bits/diabControl-client',
       url: 'https://diabcontrol.netlify.app/auth/home',
-      type: 'FullStack'
+      type: 'FullStack',
+      bgColor: "#3b82f6",
     },
     {
       name: 'Diseño de Portfolio Personal',
       description: 'Diseño de Portfolio personal enfocado en resaltar de manera adecuada mi información personal como profesional.',
       technologies: ['Angular','TypeScript'],
-      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'assets/images/projects/portfolio-app.png',
       repository: 'https://github.com/U2D-8Bits/portfolio-app',
       url: '',
-      type: 'FrontEnd'
+      type: 'FrontEnd',
+      bgColor: "#10b981",
     },
     {
       name: 'ClassBuddy',
       description: 'Aplicación web enfocada en la gestión de clases y tareas, que permite a los usuarios el generar material de estudio usando ChatGPT mejorando la experiencia de aprendizaje.',
       technologies: ['Angular', 'Kotlin', 'PostgreSQL', 'TypeScript'],
-      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'assets/images/no-image.png',
       repository: 'https://github.com/Homero22/ClassBuddy',
       url: '',
-      type: 'FullStack'
+      type: 'FullStack',
+      bgColor: "#10b981",
     },
     {
       name: 'App AeroBox',
       description: 'Plataforma web para la gestión de archivos en la nube, que permite a los usuarios almacenar y compartir archivos de manera segura y eficiente.',
       technologies: ['NextJS', 'TypeScript'],
-      image: 'https://images.unsplash.com/photo-1741802856123-04d9f001270f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: 'assets/images/projects/aerobox-app.png',
       repository: 'https://github.com/U2D-8Bits/nextjs-dashboard-app',
       url: 'https://app-aerobox.vercel.app/login',
-      type: 'FrontEnd'
+      type: 'FrontEnd',
+      bgColor: "#10b981",
     },
 
   ]
