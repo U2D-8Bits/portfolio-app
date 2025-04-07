507acaef1c8f34620fc8d15798b6debe56082585
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-07 11:07:07 -0500

    Creación del componente ToolBox con archivos HTML, CSS y TS

---
+++ src/app/presentation/ui/technologies-boxes/toolbox-box/toolbox-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbox-box',
+  imports: [],
+  templateUrl: './toolbox-box.component.html',
+  styleUrl: './toolbox-box.component.css'
+})
+export class ToolboxBoxComponent {
+
+}
