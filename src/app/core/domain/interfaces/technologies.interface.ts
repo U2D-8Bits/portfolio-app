49f98a14bd89d7ee7dad229dfaa4577ae5b1c28b
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-07 12:32:58 -0500

    Refactor TechnologiesInterface: move category and featured properties to the interface definition

--- src/app/core/domain/interfaces/technologies.interface.ts
+++ src/app/core/domain/interfaces/technologies.interface.ts
@@ -5,6 +5,10 @@
 export interface TechnologiesInterface {
   svgUrl: string;
   alt: string;
+  category: 'Frontend' | 'Backend' | 'Learning' | 'Tools';
+  featured?: boolean;
 }
+
+export type TechnologyCategory = TechnologiesInterface['category'];
 
 
