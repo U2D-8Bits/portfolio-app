32228787b33e70ac2bf9d985f0d6d3c159520849
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-07 11:06:57 -0500

    Creación del componente LearningBox con archivos HTML, CSS y TS

---
+++ src/app/presentation/ui/technologies-boxes/learning-box/learning-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-box',
+  imports: [],
+  templateUrl: './learning-box.component.html',
+  styleUrl: './learning-box.component.css'
+})
+export class LearningBoxComponent {
+
+}
