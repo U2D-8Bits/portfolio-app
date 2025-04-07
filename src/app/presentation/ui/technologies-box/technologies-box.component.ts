e2d9b7bb3d6a5bdc12101462988448dce0abe5db
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-07 11:26:42 -0500

    Crear el componente TechnologiesBox con archivos HTML, CSS y TypeScript

---
+++ src/app/presentation/ui/technologies-box/technologies-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies-box',
+  imports: [],
+  templateUrl: './technologies-box.component.html',
+  styleUrl: './technologies-box.component.css'
+})
+export class TechnologiesBoxComponent {
+
+}
