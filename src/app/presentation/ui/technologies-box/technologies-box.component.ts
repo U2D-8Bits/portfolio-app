ba1d5d4bae96999dd80f678b571b88ff372fc393
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-04-08 11:09:06 -0500

    Refactor TechnologiesBoxComponent to implement OnInit lifecycle and enhance technology categorization logic

--- src/app/presentation/ui/technologies-box/technologies-box.component.ts
+++ src/app/presentation/ui/technologies-box/technologies-box.component.ts
@@ -1,11 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+import { TechnologyService } from '../../../application/services/technology.service';
+import {
+  TechnologiesInterface,
+  TechnologyCategory,
+} from '../../../core/domain/interfaces';
 
 @Component({
   selector: 'app-technologies-box',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './technologies-box.component.html',
-  styleUrl: './technologies-box.component.css'
+  styleUrl: './technologies-box.component.css',
 })
-export class TechnologiesBoxComponent {
+export class TechnologiesBoxComponent implements OnInit {
+  //? Constructor
+  constructor(
+    private techService: TechnologyService,
+    private sanitizer: DomSanitizer
+  ) {}
 
+  //? Propiedades
+  technologies: TechnologiesInterface[] = [];
+
+  frontendTechnologies: TechnologiesInterface[] = [];
+  backendTechnologies: TechnologiesInterface[] = [];
+  learningTechnologies: TechnologiesInterface[] = [];
+  toolTechnologies: TechnologiesInterface[] = [];
+
+  categories = [
+    {
+      name: 'Frontend',
+      technologies: this.frontendTechnologies,
+    },
+    {
+      name: 'Backend',
+      technologies: this.backendTechnologies,
+    },
+    {
+      name: 'Learning',
+      technologies: this.learningTechnologies,
+    },
+    {
+      name: 'Tools',
+      technologies: this.toolTechnologies,
+    },
+  ];
+
+  //? Lifecycle Hooks
+  ngOnInit(): void {
+    this.getAllTechnologies();
+  }
+
+  //? Metodos
+
+  getAllTechnologies() {
+    this.techService.getAllTechnologies();
+    this.technologies = this.techService.getAllTechnologies();
+    this.filterTechnologiesByCategory();
+  }
+
+  //* Metodo para filtrar las tecnologias por categoria
+  filterTechnologiesByCategory() {
+    this.frontendTechnologies = this.technologies.filter(
+      (tech) => tech.category === 'Frontend'
+    );
+    this.backendTechnologies = this.technologies.filter(
+      (tech) => tech.category === 'Backend'
+    );
+    this.learningTechnologies = this.technologies.filter(
+      (tech) => tech.category === 'Learning'
+    );
+    this.toolTechnologies = this.technologies.filter(
+      (tech) => tech.category === 'Tools'
+    );
+
+    //* Asignar las tecnologias a las categorias
+    this.categories[0].technologies = this.frontendTechnologies;
+    this.categories[1].technologies = this.backendTechnologies;
+    this.categories[2].technologies = this.learningTechnologies;
+    this.categories[3].technologies = this.toolTechnologies;
+  }
+
+  getSafeSvg(svg: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(svg);
+  }
 }
